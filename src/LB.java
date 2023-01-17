866785b32ee9d67cf5fed5557fa96efa6b1a350f
Luciano Blanco <luciano.blanco@live.u-tad.com>
2023-01-17 23:16:16 +0100

    Modificacion 3

--- src/LB.java
+++ src/LB.java
@@ -2,10 +2,8 @@
 public class LB {
 	public static void main(String[] args) {
 
-		System.out.println(" Modificacion desde Master");
-		
-		
-		
+		System.out.println(" Modificacion desde Master - Cambiado");
+
 	}
-	
-} 
+
+}
